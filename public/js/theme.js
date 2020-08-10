11c49e5ea35370d09e86c2afcc86c7766a778608
fT5SgrjXeV8khTNVgQmD <4913771+y9pQnhrdix7HkhonyMXiLSo9@users.noreply.github.com>
2020-08-10 12:52:01 -0500

    Resolve Node Dependencies

---
+++ public/js/theme.js
@@ -0,0 +1,2 @@
+"use strict";(function(){var getTheme=window.localStorage&&window.localStorage.getItem("theme");var themeToggle=document.querySelector(".theme-toggle");var isDark=getTheme==="dark";if(getTheme!==null){document.body.classList.toggle("dark-theme",isDark)}themeToggle.addEventListener("click",function(){document.body.classList.toggle("dark-theme");window.localStorage&&window.localStorage.setItem("theme",document.body.classList.contains("dark-theme")?"dark":"light")})})();
+
