fdbd1fad7ae46c4e12eb3fba4a044d3ac8be89bb
fT5SgrjXeV8khTNVgQmD <4913771+simeononsecurity@users.noreply.github.com>
2020-11-09 15:10:18 -0600

    Regen Site with New Scripts

---
+++ public/scripts/standalonewindowsserver.ps1
@@ -0,0 +1,12 @@
+$github = "Standalone-Windows-Server-STIG-Script"
+$Url = "https://github.com/simeononsecurity/$github/archive/master.zip"
+$scriptname = "sos-secure-standalone-server.ps1"
+$ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
+$Destination= "C:\temp\"
+$ExtractShell = New-Object -ComObject Shell.Application
+$Files = $ExtractShell.Namespace($ZipFile).Items()
+$ExtractShell.NameSpace($Destination).CopyHere($Files)
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile
+Write-Output "Extracting ZIP..... This might take a little while"
+Start-Process $Destination\"$github"-master
+PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master\$scriptname
---
+++ public/scripts/windowsoptimizeandharden.ps1
@@ -0,0 +1,12 @@
+$github = "W10-Optimize-and-Harden"
+$Url = "https://github.com/simeononsecurity/$github/archive/master.zip"
+$scriptname = "installallstandalone.ps1"
+$ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
+$Destination= "C:\temp\"
+$ExtractShell = New-Object -ComObject Shell.Application
+$Files = $ExtractShell.Namespace($ZipFile).Items()
+$ExtractShell.NameSpace($Destination).CopyHere($Files)
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile
+Write-Output "Extracting ZIP..... This might take a little while"
+Start-Process $Destination\"$github"-master
+PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master"\$scriptname
