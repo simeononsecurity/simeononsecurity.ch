b60ff116291c2707beba3c850969bb58ca48b020
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-11-10 00:05:05 +0000

    Update standalonewindows.ps1

--- static/scripts/standalonewindows.ps1
+++ static/scripts/standalonewindows.ps1
@@ -3,10 +3,10 @@ $Url = "https://github.com/simeononsecurity/$github/archive/master.zip"
 $scriptname = "secure-standalone.ps1"
 $ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
 $Destination= "C:\temp\"
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile
 $ExtractShell = New-Object -ComObject Shell.Application
 $Files = $ExtractShell.Namespace($ZipFile).Items()
 $ExtractShell.NameSpace($Destination).CopyHere($Files) 
-Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
 Write-Output "Extracting ZIP..... This might take a little while" 
 CD $Destination\"$github"-master
-PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master\$scriptname
+PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master\"$scriptname"
