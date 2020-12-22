02c756fe825f04eaacaede2deb421474c6e3106d
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-22 02:00:05 +0000

    Update standalonewindows.ps1

--- static/scripts/standalonewindows.ps1
+++ static/scripts/standalonewindows.ps1
@@ -3,6 +3,7 @@ $Url = "https://github.com/simeononsecurity/$github/archive/master.zip"
 $scriptname = "secure-standalone.ps1"
 $ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
 $Destination= "C:\temp\"
+New-Item $Destination
 Invoke-WebRequest -Uri $Url -OutFile $ZipFile
 $ExtractShell = New-Object -ComObject Shell.Application
 $Files = $ExtractShell.Namespace($ZipFile).Items()
