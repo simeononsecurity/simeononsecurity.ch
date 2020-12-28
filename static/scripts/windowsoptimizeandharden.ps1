6af29c41bd551843cc3666edc4c65b5aa41d006a
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-28 05:15:11 +0000

    Update windowsoptimizeandharden.ps1

--- static/scripts/windowsoptimizeandharden.ps1
+++ static/scripts/windowsoptimizeandharden.ps1
@@ -3,7 +3,7 @@ $Url = "https://github.com/simeononsecurity/$github/archive/master.zip"
 $scriptname = "sos-optimize-windows.ps1"
 $ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
 $Destination= "C:\temp\"
-New-Item "C:\" -Name "temp" -ItemType "directory"
+New-Item "C:\" -Name "temp" -ItemType "directory" -Force
 Invoke-WebRequest -Uri $Url -OutFile $ZipFile
 $ExtractShell = New-Object -ComObject Shell.Application
 $Files = $ExtractShell.Namespace($ZipFile).Items()
