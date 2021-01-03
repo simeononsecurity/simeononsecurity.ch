48a3b4868b19def71b911e780923a0f806fce18c
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-01-03 00:10:27 +0000

    Update windowsoptimizeanddebloat.ps1

--- static/scripts/windowsoptimizeanddebloat.ps1
+++ static/scripts/windowsoptimizeanddebloat.ps1
@@ -10,7 +10,7 @@ $Files = $ExtractShell.Namespace($ZipFile).Items()
 Write-Output "Extracting ZIP..... This might take a little while"
 $ExtractShell.NameSpace($Destination).CopyHere($Files)
 Write-Output "Executing Script..."
-Set-Location $Destination\"$github"-master
+Set-Location $Destination\"$github"-main
 PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-main\"$scriptname"
 Write-Output "Finished"
 
