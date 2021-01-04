2ebc8c7e76ceebc143b56be3b3c18883718edf31
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-01-04 20:29:02 +0000

    Update standalonewindowsserver.ps1

--- static/scripts/standalonewindowsserver.ps1
+++ static/scripts/standalonewindowsserver.ps1
@@ -4,10 +4,14 @@ $scriptname = "sos-secure-standalone-server.ps1"
 $ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
 $Destination= "C:\temp\"
 New-Item "C:\" -Name "temp" -ItemType "directory" -Force
+Write-Host "Downloading "+"$Url"
 Invoke-WebRequest -Uri $Url -OutFile $ZipFile
 $ExtractShell = New-Object -ComObject Shell.Application
 $Files = $ExtractShell.Namespace($ZipFile).Items()
-$ExtractShell.NameSpace($Destination).CopyHere($Files)
 Write-Output "Extracting ZIP..... This might take a little while"
+$ExtractShell.NameSpace($Destination).CopyHere($Files)
+Write-Output "Executing "+"$Scriptname"
 CD $Destination\"$github"-master
 PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master\"$scriptname"
+Write-Output "Finished"
+Write-Output "Please visit https://github.com/simeononsecurity/$github/ and star the repo if this was useful for you."
