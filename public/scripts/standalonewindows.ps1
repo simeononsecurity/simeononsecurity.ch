d57d7ab6c999db731002a470d08437b6d8f808b7
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-01-04 14:50:25 -0600

    Update Scripts

--- public/scripts/standalonewindows.ps1
+++ public/scripts/standalonewindows.ps1
@@ -4,13 +4,13 @@ $scriptname = "secure-standalone.ps1"
 $ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
 $Destination= "C:\temp\"
 New-Item "C:\" -Name "temp" -ItemType "directory" -Force
-Write-Host "Downloading "+"$Url"
+Write-Host "Downloading $Url"
 Invoke-WebRequest -Uri $Url -OutFile $ZipFile
 $ExtractShell = New-Object -ComObject Shell.Application
 $Files = $ExtractShell.Namespace($ZipFile).Items()
 Write-Output "Extracting ZIP..... This might take a little while" 
 $ExtractShell.NameSpace($Destination).CopyHere($Files) 
-Write-Output "Executing "+"$Scriptname"
+Write-Output "Executing $Scriptname"
 Set-Location $Destination\"$github"-master
 PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master\"$scriptname"
 Write-Output "Finished"
--- public/scripts/windowsoptimizeanddebloat.ps1
+++ public/scripts/windowsoptimizeanddebloat.ps1
@@ -4,13 +4,13 @@ $scriptname = "sos-optimize-windows.ps1"
 $ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
 $Destination= "C:\temp\"
 New-Item "C:\" -Name "temp" -ItemType "directory" -Force
-Write-Host "Downloading "+"$Url"
+Write-Host "Downloading $Url"
 Invoke-WebRequest -Uri $Url -OutFile $ZipFile
 $ExtractShell = New-Object -ComObject Shell.Application
 $Files = $ExtractShell.Namespace($ZipFile).Items()
 Write-Output "Extracting ZIP..... This might take a little while"
 $ExtractShell.NameSpace($Destination).CopyHere($Files)
-Write-Output "Executing "+"$Scriptname"
+Write-Output "Executing $Scriptname"
 Set-Location $Destination\"$github"-main
 PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-main\"$scriptname"
 Write-Output "Finished"
