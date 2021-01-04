8b52e3e7b7f0bf07e401f55591bb2759b331b4d5
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-01-04 20:38:13 +0000

    Update standalonewindows.ps1

--- static/scripts/standalonewindows.ps1
+++ static/scripts/standalonewindows.ps1
@@ -11,7 +11,7 @@ $Files = $ExtractShell.Namespace($ZipFile).Items()
 Write-Output "Extracting ZIP..... This might take a little while" 
 $ExtractShell.NameSpace($Destination).CopyHere($Files) 
 Write-Output "Executing "+"$Scriptname"
-CD $Destination\"$github"-master
+Set-Location $Destination\"$github"-master
 PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-master\"$scriptname"
 Write-Output "Finished"
 Write-Output "Please visit https://github.com/simeononsecurity/$github/ and star the repo if this was useful for you."
