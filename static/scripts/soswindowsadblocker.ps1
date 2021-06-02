1789f5c07fe84c6a3bd51952b427df18b2d7be56
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-06-02 21:19:42 +0000

    Create soswindowsadblocker.ps1

---
+++ static/scripts/soswindowsadblocker.ps1
@@ -0,0 +1,19 @@
+$github = "System-Wide-Windows-Ad-Blocker"
+$Url = "https://github.com/simeononsecurity/$github/archive/main.zip"
+$scriptname = "sos-optimize-windows.ps1"
+$ZipFile = "C:\temp\" + $(Split-Path -Path $Url -Leaf)
+$Destination= "C:\temp\"
+New-Item "C:\" -Name "temp" -ItemType "directory" -Force
+Write-Host "Downloading $Url"
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile
+$ExtractShell = New-Object -ComObject Shell.Application
+$Files = $ExtractShell.Namespace($ZipFile).Items()
+Write-Output "Extracting ZIP..... This might take a little while"
+$ExtractShell.NameSpace($Destination).CopyHere($Files)
+Write-Output "Executing $Scriptname"
+Set-Location $Destination\"$github"-main
+PowerShell.exe -ExecutionPolicy Bypass -File C:\temp\"$github"-main\"$scriptname"
+Write-Output "Finished"
+Write-Output "Please visit https://github.com/simeononsecurity/$github/ and star the repo if this was useful for you."
+
+
