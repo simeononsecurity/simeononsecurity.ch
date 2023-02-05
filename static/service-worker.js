2109564a75bb5090c6ea2c50b98a28e1c64675fa
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-02-04 18:41:30 -0600

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -43,104 +43,33 @@ self.addEventListener('activate', (event) => {
     self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-    if (event.request.mode === 'navigate' || staticAssets.includes(event.request.url)) {
-        event.respondWith((async () => {
-            try {
-                const preloadResponse = await event.preloadResponse;
-                if (preloadResponse) {
-                    return preloadResponse;
-                }
-
-                const networkResponse = await fetch(event.request);
-                if (networkResponse.status === 200 && networkResponse.type === 'basic') {
-                    const cache = await caches.open(CACHE_NAME);
-                    cache.put(event.request, networkResponse.clone());
-                }
-                return networkResponse;
-            } catch (error) {
-                console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-                const cache = await caches.open(CACHE_NAME);
-                const cachedResponse = await cache.match(OFFLINE_URL);
-                return cachedResponse;
-            }
-        })());
-    }
-});
-
-
-self.addEventListener('push', event => {
-    if (Notification.permission === 'granted') {
-        const data = event.data.json();
-        const options = {
-            body: `A new article has been posted: ${data.title}!`,
-            badge: '/img/apple-touch-icon-192.png'
-        };
-        event.waitUntil(self.registration.showNotification('New Post', options));
-    }
-});
-
-
-
-const fetchRss = async () => {
-    const rssUrl = 'https://simeononsecurity.ch/rss.xml';
-    let parser;
-    if (typeof window !== "undefined" && typeof window.DOMParser !== "undefined") {
-        parser = new window.DOMParser();
-    } else if (typeof self !== "undefined" && typeof self.DOMParser !== "undefined") {
-        parser = new self.DOMParser();
-        try {
-            const response = await fetch(rssUrl);
-            const text = await response.text();
-            const xml = parser.parseFromString(text, "text/xml");
-            const items = xml.querySelectorAll("item");
-            const rssData = Array.from(items).map(item => {
-                const title = item.querySelector("title").textContent;
-                const link = item.querySelector("link").textContent;
-                return {
-                    title,
-                    link
-                };
+// Listen for the `fetch` event
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('https://simeononsecurity.ch')) {
+      event.respondWith(
+        fetch(event.request)
+          .then(function(response) {
+            return response.text().then(function(xmlString) {
+              const parser = new DOMParser();
+              const xml = parser.parseFromString(xmlString, 'text/xml');
+              const latestArticle = xml.querySelector('item');
+              const latestArticleTitle = latestArticle.querySelector('title').textContent;
+              const latestArticleLink = latestArticle.querySelector('link').textContent;
+              // Check if there is a new article
+              if (localStorage.getItem('latestArticleLink') !== latestArticleLink) {
+                localStorage.setItem('latestArticleLink', latestArticleLink);
+                // Show notification with the latest article title
+                self.registration.showNotification(latestArticleTitle, {
+                  body: 'A new article is available!',
+                  tag: 'new-article-notification',
+                });
+              }
+              return response;
             });
-            return rssData;
-        } catch (error) {
-            console.error(`Failed to fetch RSS data: ${error}`);
-            console.error(error.stack);
-            return null;
-        }
-    } else {
-        // handle the error here, such as logging a message or throwing an exception
-        console.error("The DOMParser is not available in this context");
+          })
+          .catch(function(error) {
+            console.error('Fetch error:', error);
+          })
+      );
     }
-};
-
-
-setInterval(async () => {
-    const rssData = await fetchRss();
-    if (rssData) {
-        const lastPost = rssData[0];
-        if (lastPost) {
-            // Check if this is a new post compared to what we have stored locally
-            const localLastPost = localStorage.getItem('lastPost');
-            if (!localLastPost || lastPost.title !== localLastPost) {
-                localStorage.setItem('lastPost', lastPost.title);
-                // Trigger the push event to show the notification
-                self.dispatchEvent(new PushEvent('push', {
-                    data: lastPost
-                }));
-            }
-        }
-    }
-}, 60000);
-
-self.addEventListener('push', event => {
-    if (Notification.permission === 'granted') {
-        const post = event.data.json();
-        const options = {
-            body: post.title,
-            badge: '/img/apple-touch-icon-192.png'
-        };
-        event.waitUntil(self.registration.showNotification('New Post', options));
-    }
-});
+  });
