d06fb95b9f98f1adc0a3571afd72075fcae45d53
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-02-16 15:49:14 -0600

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -156,3 +156,59 @@ const displayNotification = notificationTitle => {
         });
     }
 };
+
+const updateSubscriptionOnYourServer = subscription => {
+    console.log('Write your ajax code here to save the user subscription in your DB', subscription);
+    // write your own ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
+};
+
+const subscribeUser = async () => {
+    const swRegistration = await navigator.serviceWorker.getRegistration();
+    const applicationServerPublicKey = ''; // paste your webpush certificate public key
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey
+    })
+    .then((subscription) => {
+        console.log('User is subscribed newly:', subscription);
+        updateSubscriptionOnServer(subscription);
+    })
+    .catch((err) => {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied')
+        } else {
+          console.error('Failed to subscribe the user: ', err)
+        }
+    });
+};
+const urlB64ToUint8Array = (base64String) => {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4)
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/')
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+};
+
+const checkSubscription = async () => {
+    const swRegistration = await navigator.serviceWorker.getRegistration();
+    swRegistration.pushManager.getSubscription()
+    .then(subscription => {
+        if (!!subscription) {
+            console.log('User IS Already subscribed.');
+            updateSubscriptionOnYourServer(subscription);
+        } else {
+            console.log('User is NOT subscribed. Subscribe user newly');
+            subscribeUser();
+        }
+    });
+};
+
+checkSubscription();
