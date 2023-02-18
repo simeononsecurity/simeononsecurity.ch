7d6763747e2ff7d68ebc564ff102b16eb5476239
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-02-18 01:34:36 -0600

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -122,101 +122,101 @@ setInterval(async () => {
 
 // https://geekflare.com/convert-webapp-to-pwa/
 
-self.addEventListener('push', event => {
-    const data = event.data.json();
-    const options = {
-        body: `${data.title}`,
-        badge: '/img/apple-touch-icon-192.png'
-    };
-    event.waitUntil(self.registration.showNotification('New Post', options));
-});
+// self.addEventListener('push', event => {
+//     const data = event.data.json();
+//     const options = {
+//         body: `${data.title}`,
+//         badge: '/img/apple-touch-icon-192.png'
+//     };
+//     event.waitUntil(self.registration.showNotification('New Post', options));
+// });
 
-if ('Notification' in window && Notification.permission != 'granted') {
-    console.log('Ask user permission')
-    Notification.requestPermission(status => {  
-        console.log('Status:'+status)
-        displayNotification('Notification Enabled');
-    });
-}
+// if ('Notification' in window && Notification.permission != 'granted') {
+//     console.log('Ask user permission')
+//     Notification.requestPermission(status => {  
+//         console.log('Status:'+status)
+//         displayNotification('Notification Enabled');
+//     });
+// }
 
-const displayNotification = notificationTitle => {
-    console.log('display notification')
-    if (Notification.permission == 'granted') {
-        navigator.serviceWorker.getRegistration().then(reg => {
-            console.log(reg)
-            const options = {
-                    body: 'Thanks for allowing push notification !',
-                    icon:  '/img/apple-touch-icon-144-precomposed.png',
-                    vibrate: [100, 50, 100],
-                    data: {
-                      dateOfArrival: Date.now(),
-                      primaryKey: 0
-                    }
-                  };
+// const displayNotification = notificationTitle => {
+//     console.log('display notification')
+//     if (Notification.permission == 'granted') {
+//         navigator.serviceWorker.getRegistration().then(reg => {
+//             console.log(reg)
+//             const options = {
+//                     body: 'Thanks for allowing push notification !',
+//                     icon:  '/img/apple-touch-icon-144-precomposed.png',
+//                     vibrate: [100, 50, 100],
+//                     data: {
+//                       dateOfArrival: Date.now(),
+//                       primaryKey: 0
+//                     }
+//                   };
     
-            reg.showNotification(notificationTitle, options);
-        });
-    }
-};
+//             reg.showNotification(notificationTitle, options);
+//         });
+//     }
+// };
 
-const updateSubscriptionOnYourServer = subscription => {
-    console.log('Write your ajax code here to save the user subscription in your DB', subscription);
-    // write your own ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
-};
+// const updateSubscriptionOnYourServer = subscription => {
+//     console.log('Write your ajax code here to save the user subscription in your DB', subscription);
+//     // write your own ajax request method using fetch, jquery, axios to save the subscription in your server for later use.
+// };
 
-const subscribeUser = async () => {
-    const swRegistration = await navigator.serviceWorker.getRegistration();
-    const applicationServerPublicKey = ''; // paste your webpush certificate public key
-    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
-    swRegistration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey
-    })
-    .then((subscription) => {
-        console.log('User is subscribed newly:', subscription);
-        updateSubscriptionOnServer(subscription);
-    })
-    .catch((err) => {
-        if (Notification.permission === 'denied') {
-          console.warn('Permission for notifications was denied')
-        } else {
-          console.error('Failed to subscribe the user: ', err)
-        }
-    });
-};
-const urlB64ToUint8Array = (base64String) => {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4)
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/')
+// const subscribeUser = async () => {
+//     const swRegistration = await navigator.serviceWorker.getRegistration();
+//     const applicationServerPublicKey = ''; // paste your webpush certificate public key
+//     const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+//     swRegistration.pushManager.subscribe({
+//       userVisibleOnly: true,
+//       applicationServerKey
+//     })
+//     .then((subscription) => {
+//         console.log('User is subscribed newly:', subscription);
+//         updateSubscriptionOnServer(subscription);
+//     })
+//     .catch((err) => {
+//         if (Notification.permission === 'denied') {
+//           console.warn('Permission for notifications was denied')
+//         } else {
+//           console.error('Failed to subscribe the user: ', err)
+//         }
+//     });
+// };
+// const urlB64ToUint8Array = (base64String) => {
+//     const padding = '='.repeat((4 - base64String.length % 4) % 4)
+//     const base64 = (base64String + padding)
+//         .replace(/\-/g, '+')
+//         .replace(/_/g, '/')
 
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
+//     const rawData = window.atob(base64);
+//     const outputArray = new Uint8Array(rawData.length);
 
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-};
+//     for (let i = 0; i < rawData.length; ++i) {
+//         outputArray[i] = rawData.charCodeAt(i);
+//     }
+//     return outputArray;
+// };
 
-const checkSubscription = async () => {
-    const swRegistration = await navigator.serviceWorker.getRegistration();
-    swRegistration.pushManager.getSubscription()
-    .then(subscription => {
-        if (!!subscription) {
-            console.log('User IS Already subscribed.');
-            updateSubscriptionOnYourServer(subscription);
-        } else {
-            console.log('User is NOT subscribed. Subscribe user newly');
-            subscribeUser();
-        }
-    });
-};
+// const checkSubscription = async () => {
+//     const swRegistration = await navigator.serviceWorker.getRegistration();
+//     swRegistration.pushManager.getSubscription()
+//     .then(subscription => {
+//         if (!!subscription) {
+//             console.log('User IS Already subscribed.');
+//             updateSubscriptionOnYourServer(subscription);
+//         } else {
+//             console.log('User is NOT subscribed. Subscribe user newly');
+//             subscribeUser();
+//         }
+//     });
+// };
 
-checkSubscription();
+// checkSubscription();
 
-self.addEventListener('push', (event) => {
-    const json = JSON.parse(event.data.text())
-    console.log('Push Data', event.data.text())
-    self.registration.showNotification(json.header, json.options)
-  });
+// self.addEventListener('push', (event) => {
+//     const json = JSON.parse(event.data.text())
+//     console.log('Push Data', event.data.text())
+//     self.registration.showNotification(json.header, json.options)
+//   });
