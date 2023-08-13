591ed50574537cfc4da214a7a0246bc0b4956d1d
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-08-13 01:20:33 -0500

    adjustments

--- generate_md/generate_md.js
+++ generate_md/generate_md.js
@@ -2,32 +2,32 @@ const fs = require('fs');
 const axios = require('axios');
 const xml2js = require('xml2js');
 
-async function fetchSitemap(url) {
+async function fetchRssFeed(url) {
   const response = await axios.get(url);
   return response.data;
 }
 
-async function parseSitemap(sitemapXml) {
+async function parseRssFeed(rssXml) {
   const parser = new xml2js.Parser();
-  const parsedData = await parser.parseStringPromise(sitemapXml);
-  return parsedData.urlset.url;
+  const parsedData = await parser.parseStringPromise(rssXml);
+  return parsedData.rss.channel[0].item;
 }
 
-function generateMarkdown(urls) {
-  const markdownContent = urls.map(urlData => {
-    const title = urlData['news:title'][0];
-    const url = urlData.loc[0];
+function generateMarkdown(items) {
+  const markdownContent = items.map(item => {
+    const title = item.title[0];
+    const url = item.link[0];
     return `- [${title}](${url})`;
   }).join('\n');
 
-  fs.writeFileSync('sitemap_links.md', markdownContent);
+  fs.writeFileSync('rss_links.md', markdownContent);
 }
 
 async function run() {
-  const sitemapUrl = 'https://simeononsecurity.ch/sitemap.xml';
-  const sitemapXml = await fetchSitemap(sitemapUrl);
-  const parsedUrls = await parseSitemap(sitemapXml);
-  generateMarkdown(parsedUrls);
+  const rssUrl = 'https://simeononsecurity.ch/index.xml';
+  const rssXml = await fetchRssFeed(rssUrl);
+  const parsedItems = await parseRssFeed(rssXml);
+  generateMarkdown(parsedItems);
 }
 
 run().catch(error => {
