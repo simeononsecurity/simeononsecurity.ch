a8f9331079826cda09cf470686c4f67f77347661
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-10-12 04:03:30 -0500

    Revert "Update search.js"
    This reverts commit 7dc137ccbd2f8b1bb829286ad452469140aada73.

--- static/js/search.js
+++ static/js/search.js
@@ -6,9 +6,9 @@ const fuseOptions = {
   includeMatches: true,
   includeScore: true,
   tokenize: true,
-  location: 0.10,
+  location: 0,
   distance: 100,
-  minMatchCharLength: 5,
+  minMatchCharLength: 1,
   keys: [
     {name: "title", weight: 0.45},
     {name: "contents", weight: 0.6},
