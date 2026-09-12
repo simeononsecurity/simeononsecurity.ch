b868352376c76dc03079f21502164ba8fe5d93f6
simeononsecurity <githubUUQ5arnYZZMCfrutbXmGh5mq@simeonus.33mail.com>
2026-09-12 23:17:22 +0000

    Update website-nl branch

--- assets/img/cover/compresseddownload.ps1
+++
@@ -1,7 +0,0 @@
-foreach ($file in (get-childitem)){
-    $URL = "https://simeononsecurity.ch/img/cover/$file"
-    $Path = $file.fullname
-    Write-Host $URL
-    Write-Host $Path
-    Invoke-WebRequest -URI $URL -OutFile $Path
-}
